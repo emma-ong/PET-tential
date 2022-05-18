d79f7463af5ae5245b0af27db6dfef25604cd226
emma-ong <emmaong.at@gmail.com>
2022-05-18 17:05:47 +1200

    Configure Auth0

--- client/index.js
+++ client/index.js
@@ -3,9 +3,22 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux';
 import { BrowserRouter  } from "react-router-dom";
 import store from './store'
-
 import App from './components/App'
 
+import { Auth0Provider } from "@auth0/auth0-react";
+
 document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(<Provider store={store}> <BrowserRouter ><App /></BrowserRouter></Provider>, document.getElementById('app'))
+  ReactDOM.render(
+    <Auth0Provider
+    domain="https://pets/api"
+    clientId="UfmFgK7k0mAFp0QSo2Aa2tttedDYXmE8"
+    redirectUri={window.location.origin}
+  >  
+    <Provider store={store}> 
+      <BrowserRouter >
+        <App />
+      </BrowserRouter>
+    </Provider>
+    </Auth0Provider>
+  , document.getElementById('app'))
 })
