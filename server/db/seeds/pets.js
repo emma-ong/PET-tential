d2bca729c1bc5146a7c28cd8b789a703daa62c64
emma-ong <emmaong.at@gmail.com>
2022-05-21 22:13:09 +1200

    Make seeds images all placeholders

--- server/db/seeds/pets.js
+++ server/db/seeds/pets.js
@@ -6,7 +6,7 @@ exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('pets').del()
   await knex('pets').insert([
-    {id: 1, name: 'Snoop', mobile: 2139905567, description: "Loves high spaces and catnip", img: 'snoop.jpg'},
-    {id: 2, name: 'Cardi Barks', mobile: 2139905567, description: "Loves a good butt scratch", img: 'cardi.jpg' },
+    {id: 1, name: 'Snoop', mobile: 2139905567, description: "Loves high spaces and catnip", img: 'pet-tential.png'},
+    {id: 2, name: 'Cardi Barks', mobile: 2139905567, description: "Loves a good butt scratch", img: 'pet-tential.png' },
   ]);
 };
